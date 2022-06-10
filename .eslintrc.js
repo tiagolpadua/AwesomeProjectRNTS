b4682cd9cdc3c5627eb4b0c7560c5e148a10cf14
Tiago <tiagolpadua@gmail.com>
2022-06-10 12:13:04 -0300

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
+        'object-curly-spacing': ['error', 'always'],
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import {NativeBaseProvider, Box} from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Box, NativeBaseProvider } from 'native-base';
 import React from 'react';
 
-const App = () => {
+function HomeScreen() {
   return (
     <NativeBaseProvider>
       <Box>Hello world</Box>
     </NativeBaseProvider>
   );
-};
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
