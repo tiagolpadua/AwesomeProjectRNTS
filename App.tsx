983b985c62f1200e10bebdcfa14b0cf18be25d11
Tiago <tiagolpadua@gmail.com>
2022-06-10 11:39:09 -0300

    json server

--- App.tsx
+++ App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import {NativeBaseProvider, Box} from 'native-base';
 import React from 'react';
 
